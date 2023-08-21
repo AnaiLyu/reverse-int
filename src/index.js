8eb41a506ef45798c49e22f70ae03442eb475b9a
AnaiLyu <lyuanai88@gmail.com>
2023-08-21 23:38:43 +0300

    Complete the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let numToStr = n.toString();
+    if (numToStr[0] === "-") {
+        numToStr = numToStr.replace("-", "");
+    }
+    let resalt = "";
+    for (let i = 0; i < numToStr.length; i++) {
+        resalt = `${numToStr[i]}${resalt}`;
+    }
+    return +resalt;
+};
