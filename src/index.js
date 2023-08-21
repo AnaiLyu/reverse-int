4b33b532d9b94d9b189c52459994622e01eaa34b
AnaiLyu <lyuanai88@gmail.com>
2023-08-21 23:49:28 +0300

    Improve task solutions

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
-module.exports = function reverse(n) {
-    let numToStr = n.toString();
-    if (numToStr[0] === "-") {
-        numToStr = numToStr.replace("-", "");
+module.exports = reverse = (n) => {
+    if (n[0] === "-") {
+        n = n.replace("-", "");
     }
-    let resalt = "";
-    for (let i = 0; i < numToStr.length; i++) {
-        resalt = `${numToStr[i]}${resalt}`;
-    }
-    return +resalt;
+    n.toString().split("").reverse().join("");
 };
